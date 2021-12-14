d538d18829235d49cbb1b74085cf463aaa77877f
Matt Huang <r06245017@ntu.edu.tw>
2021-12-14 17:30:58 +0800

    20211214 deleted files

--- client/src/store/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from "redux"
-import login from './login'
-import password from './password'
-const rootReducer = combineReducers({
-    login,
-    password,
-})
-
-export default rootReducer
