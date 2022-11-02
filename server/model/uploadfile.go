5c83848cb243330ab151457b957c0f59f46c17a6
wenlianghuang <wenliangmatt@gmail.com>
2022-11-03 06:33:47 +0800

    new file model/uploadfile.go

---
+++ server/model/uploadfile.go
@@ -0,0 +1,42 @@
+package model
+
+import (
+	"crypto/md5"
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+// 處理/upload 邏輯
+func Uploadfile(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //取得請求的方法
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("upload.gtpl")
+		t.Execute(w, token)
+	} else {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666) // 此處假設當前目錄下已存在 test 目錄
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	}
+}
