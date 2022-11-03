fba03a857b68fcc3ed818d5203e03127e251ccb8
wenlianghuang <wenliangmatt@gmail.com>
2022-11-04 06:59:44 +0800

    modified file model/uploadfile.go

--- server/model/uploadfile.go
+++ server/model/uploadfile.go
@@ -1,42 +1,65 @@
 package model
 
 import (
-	"crypto/md5"
 	"fmt"
-	"html/template"
-	"io"
+	"io/ioutil"
 	"net/http"
-	"os"
-	"strconv"
-	"time"
 )
 
 // 處理/upload 邏輯
 func Uploadfile(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method) //取得請求的方法
-	if r.Method == "GET" {
-		crutime := time.Now().Unix()
-		h := md5.New()
-		io.WriteString(h, strconv.FormatInt(crutime, 10))
-		token := fmt.Sprintf("%x", h.Sum(nil))
+	/*
+		fmt.Println("method:", r.Method) //取得請求的方法
+		if r.Method == "GET" {
+			crutime := time.Now().Unix()
+			h := md5.New()
+			io.WriteString(h, strconv.FormatInt(crutime, 10))
+			token := fmt.Sprintf("%x", h.Sum(nil))
 
-		t, _ := template.ParseFiles("upload.gtpl")
-		t.Execute(w, token)
-	} else {
-		r.ParseMultipartForm(32 << 20)
-		file, handler, err := r.FormFile("uploadfile")
-		if err != nil {
-			fmt.Println(err)
-			return
+			t, _ := template.ParseFiles("upload.gtpl")
+			t.Execute(w, token)
+		} else {
+			r.ParseMultipartForm(32 << 20)
+			file, handler, err := r.FormFile("myFile")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer file.Close()
+			fmt.Fprintf(w, "%v", handler.Header)
+			f, err := os.OpenFile("D:/managerxmodulexonline/server/test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666) // 此處假設當前目錄下已存在 test 目錄
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer f.Close()
+			io.Copy(f, file)
 		}
-		defer file.Close()
-		fmt.Fprintf(w, "%v", handler.Header)
-		f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666) // 此處假設當前目錄下已存在 test 目錄
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer f.Close()
-		io.Copy(f, file)
+	*/
+	fmt.Println("File Upload Endpoint Hit")
+	r.ParseMultipartForm(10)
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
 	}
+
+	defer file.Close()
+	fmt.Printf("Upload File %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	tempFile, err := ioutil.TempFile("D:/temp-image/", "upload-*.png")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer tempFile.Close()
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	tempFile.Write(fileBytes)
+	fmt.Fprintf(w, "Successfully")
 }
