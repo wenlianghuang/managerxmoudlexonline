c12894d1d56f91244f0fae3f4085251cc65c381f
wenlianghuang <wenliangmatt@gmail.com>
2022-11-04 06:58:08 +0800

    modified file src/components/UploadFile/UploadFile.js

--- client/src/components/UploadFile/UploadFile.js
+++ client/src/components/UploadFile/UploadFile.js
@@ -1,15 +1,50 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import Stack from '@mui/material/Stack';
+import React,{useState} from 'react'
+import axios from 'axios'
 
-export default function UploadFile() {
-    return (
-        <Stack direction="row" alignItems="center" spacing={2}>
-            <Button variant='contained' component="label">
-                Upload 
-                <input hidden accept='image/*' type="file"/>
-            </Button>
-        </Stack>
+export default function UploadFile(){
+    const [selectedFile,setSelectedFile] = useState(null)
+    const formData = new FormData()
+    formData.append("selectedFile",selectedFile)
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        const formData = new FormData()
+        formData.append("selectedFile",selectedFile)
+        /*
+        try{
+            axios({
+                method: "post",
+                url:"/upload",
+                data:formData,
+                headers:{"Content-Type":"multipart/form-data"},
+            })
+            
+        }catch(error){
+            console.log(error)
+        }
+        */
+        await axios.post("/upload",formData,
+          {
+            headers:{
+              "Content-Type": "application/json",
+            },
+          }
+          ).then((res)=>{
+            console.log(res.data)
+            
+            
+          }).catch((error)=>{
+            console.error(error)
+          })
+    }
+    const handleFileSelect = (event) => {
+        setSelectedFile(event.target.files[0])
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input type="file" onChange={handleFileSelect}/>
+            <input type="submit" value="Upload File"/>
+        </form>
     )
 }
