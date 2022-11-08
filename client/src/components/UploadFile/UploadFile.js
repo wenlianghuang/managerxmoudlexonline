3484fc4374840bcd0a0fdefaf99062a24c56f70a
wenlianghuang <wenliangmatt@gmail.com>
2022-11-08 22:33:02 +0800

    modified file of frontend UploadFile/UploadFile.js

--- client/src/components/UploadFile/UploadFile.js
+++ client/src/components/UploadFile/UploadFile.js
@@ -3,41 +3,43 @@ import axios from 'axios'
 
 export default function UploadFile(){
     const [selectedFile,setSelectedFile] = useState(null)
-    const formData = new FormData()
-    formData.append("selectedFile",selectedFile)
+    //const formData = new FormData()
+    //formData.append("selectedFile",selectedFile)
 
     const handleSubmit = async (event) => {
         event.preventDefault()
         const formData = new FormData()
         formData.append("selectedFile",selectedFile)
         /*
-        try{
-            axios({
-                method: "post",
-                url:"/upload",
-                data:formData,
-                headers:{"Content-Type":"multipart/form-data"},
-            })
-            
-        }catch(error){
+        try {
+            const response = await axios({
+              method: "post",
+              url: "/upload",
+              data: formData,
+              headers: { "Content-Type": "multipart/form-data" },
+            });
+          } catch(error) {
             console.log(error)
-        }
+          }
         */
+       
         await axios.post("/upload",formData,
           {
             headers:{
-              "Content-Type": "application/json",
+              "Content-Type": "multipart/form-data",
             },
           }
           ).then((res)=>{
-            console.log(res.data)
+            console.log("Success")
             
             
           }).catch((error)=>{
-            console.error(error)
+            console.error("Failed: ",error)
           })
+        
     }
     const handleFileSelect = (event) => {
+        console.log(event.target.files);
         setSelectedFile(event.target.files[0])
     }
 
